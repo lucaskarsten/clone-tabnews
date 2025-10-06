3a44a4b6ae81f7c5f4c431771c5e7aa4e031d566
lucaskarsten <57293107+lucaskarsten@users.noreply.github.com>
2025-10-06 01:53:42 +0000

    adicionado arquivo `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ status: "ok" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database("SELECT 1 + 1 AS result");
+  console.log(result.rows);
+  response.status(200).json({ result: result.rows[0].result });
 }
 
 export default status;
