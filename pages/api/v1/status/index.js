39dcc24937aae8e43ad9f5c29e5113d223938b02
lucaskarsten <57293107+lucaskarsten@users.noreply.github.com>
2025-10-07 00:50:18 +0000

    Finalizado endpoint `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,40 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database("SELECT 1 + 1 AS result");
-  console.log(result.rows);
-  response.status(200).json({ result: result.rows[0].result });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databasOepenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databasOepenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connection: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
+
+// versao do postres
+// conexao maxima
+// conexao usada
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Check if it's a valid date
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100); // Default max_connections for PostgreSQL
+  expect(responseBody.dependencies.database.open_connection).toEqual(1);
+
+  //expect(responseBody.postgres_version).toBeDefined();
+  //expect(responseBody.max_connections).toBeDefined();
+  //expect(responseBody.current_connections).toBeDefined();
 });
