2009999f7fa0b0a88dc5dd04d508716a606dff45
Lucas Karsten <57293107+lucaskarsten@users.noreply.github.com>
2026-01-06 13:37:17 +0000

    alterado index para namorada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 function Home() {
-  return <h1>Isso ainda será um grande projeto.</h1>;
+  return (
+    <div>
+      <h1>Te amo.</h1>
+      <h1>Obrigado por tudo</h1>
+
+      <Image
+        src="https://i.imgur.com/acasM70.jpeg"
+        alt="foto"
+        width={720}
+        height={1280}
+      />
+    </div>
+  );
 }
 
 export default Home;
